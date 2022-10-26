4deac0adf6413c9105213f2ac80257c217253675
Jéssica <jessicapmaximo@gmail.com>
2022-10-26 13:20:44 -0300

    desafio 1

---
+++ Fundamentos/Introdução à JavaScript/exercicios_dia4/desafio2.js
@@ -0,0 +1,8 @@
+Exercício 2 - Crie uma função que, dado um array de números inteiros,
+// retorne a quantidade de números pares e ímpares no formato abaixo:
+
+let numeros = {
+  pares: 0,
+  ímpares: 0,
+};
+
