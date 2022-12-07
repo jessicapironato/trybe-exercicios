93a328f13672a74f3bd2f8eaf0ca1f96509a54af
Jéssica <jessicapmaximo@gmail.com>
2022-12-07 16:20:24 -0300

    dia5

---
+++ Fundamentos/HOFS/DIA5/exemplo1.js
@@ -0,0 +1,51 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2]) // 54
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+const [firstPrime, secundPrime, thirdPrime] = primeNumbers
+sum(firstPrime, thirdPrime)
+//----------------------------------------
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida, animal, bebida] = [bebida, comida, animal]; // Criando o array e o desestruturando
+console.log(comida, animal, bebida); // arroz gato água
+
+//____________________
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+[,,,...numerosPares] = numerosPares;
+console.log(numerosPares)
+
+//----------------------------
+
+const getNationality = ({ firstName, nationality = 'seiLá' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+const {nationality = 'seiLá'} = person
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
